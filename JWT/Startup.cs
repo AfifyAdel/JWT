745e29031855ccd2728a144559438f8317070082
Afify Adel <afifyadel25@gmail.com>
2021-01-02 21:38:42 +0200

    Add services & configuration for jwt authentication

--- JWT/Startup.cs
+++ JWT/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JWT.BusinessLogic.Services;
+using JWT.Domain.Models;
+using JWT.Domain.Services;
+using JWT.Factories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +29,22 @@ namespace JWT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder =>
+                    {
+                        builder.WithOrigins(Configuration.GetValue<string>("siteCors").Split(";"))
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    });
+            });
             services.AddControllers();
+            services.Configure<TokenManagement>(Configuration.GetSection("tokenManagement"));
+            services.AddJwtAuthentication(Configuration);
+
+            services.AddSingleton<ITokenService, TokenService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +58,12 @@ namespace JWT
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
